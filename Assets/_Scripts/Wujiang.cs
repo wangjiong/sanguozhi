3f97f9c3839ecb785ac5c90cbbcf0c1cf3d3c493
wangjiong <995204127@qq.com>
2017-11-27 00:07:18 +0800

    1.增加Game场景

---
+++ Assets/_Scripts/Wujiang.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Wujiang : MonoBehaviour , IPointerClickHandler{
+
+    public void OnPointerClick(PointerEventData eventData) {
+        print("eventData");
+    }
+
+    void Start () {
+		
+	}
+
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			//print (tag + "GetMouseButton");
+		}
+	}
+}
