4396125850ffdaaffcac4bac07fb45f75f1c558f
wangjiong <995204127@163.com>
2017-11-30 21:34:26 +0800

    1.增加地形表格

---
+++ Assets/_Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Camera.main.transform);
+        //transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y * 1, 0);
+    }
+}
--- Assets/_Scripts/Wujiang.cs
+++ Assets/_Scripts/Wujiang.cs
@@ -1,25 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TGS;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Wujiang : MonoBehaviour , IPointerClickHandler{
-    HighlightableObject ho;
-    bool selected;
-    public void OnPointerClick(PointerEventData eventData) {
-        print("eventData");
-        if (ho == null) {
-            ho = gameObject.AddComponent<HighlightableObject>();
+public class Wujiang : MonoBehaviour {
+    HighlightableObject mHighlightableObjecto;
+    bool mSelected;
+
+    TerrainGridSystem tgs;
+    public int index = 0;
+    public Vector2 location = Vector2.zero;
+
+    float timer = 0.2f;
+
+    void Start() {
+        tgs = TerrainGridSystem.instance;
+        location.x = tgs.columnCount / 2;
+        location.y = tgs.rowCount / 2;
+
+        location.x++;
+        if (location.x >= tgs.columnCount) {
+            location.x = 0;
+            location.y++;
+            if (location.y >= tgs.rowCount) {
+                location.y = 0;
+            }
         }
-        selected = !selected;
-        if (selected) {
-            ho.ConstantOnImmediate(Color.red);
-        } else {
-            ho.Off();
+        Cell cell = tgs.CellGetAtPosition((int)location.x, (int)location.y);
+        int cellIndex = tgs.CellGetIndex(cell);
+        if (cellIndex >= 0) {
+            Vector3 temp = tgs.CellGetPosition(cellIndex);
+            transform.position = new Vector3(temp.x, transform.position.y, temp.z);
+        }
+        timer = 2.0f;
+    }
+
+    void Update() {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+
+            //Use these lines to trace by column and row.
+            location.x++;
+            if (location.x >= tgs.columnCount) {
+                location.x = 0;
+                location.y++;
+                if (location.y >= tgs.rowCount) {
+                    location.y = 0;
+                }
+            }
+            Cell cell = tgs.CellGetAtPosition((int)location.x, (int)location.y);
+            int cellIndex = tgs.CellGetIndex(cell);
+            if (cellIndex >= 0)
+                transform.position = tgs.CellGetPosition(cellIndex);
+
+
+            timer = 2.0f;
         }
     }
 
-    void Start () {
-		
-	}
+    void OnMouseDown() {
+        if (mHighlightableObjecto == null) {
+            mHighlightableObjecto = gameObject.AddComponent<HighlightableObject>();
+        }
+        mSelected = !mSelected;
+        if (mSelected) {
+            mHighlightableObjecto.ConstantOnImmediate(Color.red);
+        } else {
+            mHighlightableObjecto.Off();
+        }
+    }
 }
