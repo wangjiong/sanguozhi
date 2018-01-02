6c2f66a9eb6c9011a63593e0181e0b205b1365ea
wangjiong <995204127@163.com>
2018-01-02 17:48:35 +0800

    1.增加出征界面

--- Assets/_Scripts/GameMenu/MenuFirst.cs
+++ Assets/_Scripts/GameMenu/MenuFirst.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MsunFirst : MonoBehaviour {
+public class MenuFirst : MonoBehaviour {
 
     public GameObject mSecondMeun;
 
---
+++ Assets/_Scripts/GameMenu/MenuSecond.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSecond : MonoBehaviour {
+
+    public GameObject mExpeditionMeun;
+
+    public void ShowEcpedition() {
+        mExpeditionMeun.SetActive(true);
+    }
+
+    public void HideEcpedition() {
+        mExpeditionMeun.SetActive(false);
+    }
+
+}
