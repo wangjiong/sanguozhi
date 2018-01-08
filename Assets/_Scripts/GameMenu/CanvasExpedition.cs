0e29263f81408d356501c4c98fbb5e420afd19c1
wangjiong <995204127@163.com>
2018-01-08 19:55:28 +0800

    1.调整UI

--- Assets/_Scripts/GameMenu/CanvasExpedition.cs
+++ Assets/_Scripts/GameMenu/CanvasExpedition.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CanvasExpedition : MonoBehaviour {
-
+    static string TAG = "CanvasExpedition==";
     public GameObject mExecutiveBtn;
     public GameObject mEnableBtn;
     public GameObject mCloseBtn;
     public GameObject mCanvasExecutive;
+
+    public Button[] mWeapons;
+    public Sprite[] mSprite01; // 普通
+    public Sprite[] mSprite02; // 高亮
+    public int mWeaponIndex = 0;
 
     void Start() {
         mExecutiveBtn.GetComponent<Button>().onClick.AddListener(delegate () {
@@ -20,6 +25,20 @@ public class CanvasExpedition : MonoBehaviour {
         mCloseBtn.GetComponent<Button>().onClick.AddListener(delegate () {
             this.gameObject.SetActive(false);
         });
+        // 默认第一个亮
+        mWeapons[mWeaponIndex].GetComponent<Image>().sprite = mSprite02[mWeaponIndex];
+        for (int i = 0; i < mWeapons.Length; i++) {
+            int index = i;
+            mWeapons[index].onClick.AddListener(delegate () {
+                if (mWeaponIndex == index) {
+                    return;
+                }else {
+                    mWeapons[mWeaponIndex].GetComponent<Image>().sprite = mSprite01[mWeaponIndex];
+                    mWeaponIndex = index;
+                    mWeapons[mWeaponIndex].GetComponent<Image>().sprite = mSprite02[mWeaponIndex];
+                };
+            });
+        }
     }
 
 }
